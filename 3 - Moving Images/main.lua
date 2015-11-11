8f1fc998d1ab55bf2c77935953426f1112b38136
la280 <laura.aubin@ocsbstudent.ca>
2015-11-11 10:30:21 -0500

    Update
    Added images and start file

---
+++ 3 - Moving Images/main.lua
@@ -0,0 +1,13 @@
+-- 3- Moving Images
+
+-- Created by Laura Aubin 
+
+-- Created on November 11, 2015
+
+-- This program moves an image around the screen, controlling speed and
+
+-- Set game settings
+display.setStatusBar(display.HiddenStatusBar)
+
+-- Local variables (where, wide, height)
+local backgroundImage = display.newImageRect("./Images/background.png", 100, 200)
